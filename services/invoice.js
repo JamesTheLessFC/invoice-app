c089cea38be4e84158c65bd33c8f760774539684
James F Ciskanik <ciskanikjf@gmail.com>
2021-11-16 01:30:11 -0500

    change rtk query baseurl to production url

--- services/invoice.js
+++ services/invoice.js
@@ -4,7 +4,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define a service using a base URL and expected endpoints
 export const invoiceApi = createApi({
   reducerPath: "invoiceApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://invoicer-five.vercel.app/api/",
+  }),
   tagTypes: ["Invoices"],
   endpoints: (builder) => ({
     getInvoices: builder.query({
